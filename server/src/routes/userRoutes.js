2678aad52b79e9a4e805b17d6eb9475d14d5bad3
carl256 <lukhobacarljulius@gmail.com>
2023-04-19 20:24:17 +0200

    removed unwanted extra lines

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,20 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController');
-const errorHanderMiddleware = require('../middleware/authErrors/userErrorHandlers');
-const { sanitizeLoginInput } = require('../middleware/authentication/userAuth');
+const userController = require("../controllers/userController");
+const errorHanderMiddleware = require("../middleware/authErrors/userErrorHandlers");
+const { sanitizeLoginInput } = require("../middleware/authentication/userAuth");
 
 const { login, signup, logout } = userController;
 
 const { handleLoginError, handleSignupError } = errorHanderMiddleware;
 
-router.post('/login', sanitizeLoginInput, login);
-router.post('/signup', signup);
-// router.post('/logout', logout);
+router.post("/login", sanitizeLoginInput, login);
+router.post("/signup", signup);
 
 // // use the error handler middleware
 router.use(handleLoginError, handleSignupError);
-
-
 
 module.exports = router;
